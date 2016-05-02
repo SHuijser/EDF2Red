0184d6796da53dae9be715f3d5e67d3c6cf55e6a
SHuijser <Kralkatorrik@ai17870.ai.rug.nl>
2016-05-02 17:17:24 +0200

    printf->sprint

--- src/readEDF.cpp
+++ src/readEDF.cpp
@@ -23,7 +23,7 @@ DataFrame parseEDFevents(std::string fname) {
   if (ed != NULL) {
     /* Command to return the number of elements in the opened EDF file. */
     sampleCount = edf_get_element_count(ed);
-    printf('%d', sampleCount);
+    sprintf("%d", sampleCount);
   }
 
   // Create the lists which we're going to return as a data.frame:
